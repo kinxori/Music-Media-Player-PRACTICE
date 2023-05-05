1795f77d1cf86305911879f968da03dee638c8df
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-05 17:29:12 -0600

    replacing icon buttons for imgs

--- src/App.tsx
+++ src/App.tsx
@@ -11,37 +11,39 @@ function App() {
         <div className="song-content bg-zinc-600 w-[50%] h-[100%] m-0 flex justify-center items-center flex-col">
           <div className="song-copy m-0  h-[60%] w-[100%]">
             <button className="song-copy h-min w-min ml-auto flex">
-              <i className="fa-solid fa-circle my-[10px] mx-[10px] text-[30px]"></i>
+              <i className="fa-solid fa-music my-[10px] mx-[10px] text-[30px]"></i>
             </button>
             <h2 className="text-[30px] font-bold mt-[20px] px-[20px]">Song</h2>
             <h3 className="text-[18px] italic px-[20px]">artist</h3>
           </div>
           <div className="song-range m-0 h-[20%] w-[100%] flex justify-center items-center">
             <button className="h-min w-min flex">
-              <i className="fa-solid fa-circle mx-[20px] text-[16px]"></i>
+              <i className="fa-regular fa-thumbs-down mx-[20px] text-[16px]"></i>
+              {/* <i class="fa-solid fa-thumbs-down"></i> */}
             </button>
             <span className="mx-[10px] text-[12px]">00:00</span>
             <input type="range"></input>
             <span className="mx-[10px] text-[12px]">00:00</span>
             <button>
-              <i className="fa-solid fa-circle mx-[20px] text-[16px]"></i>
+              <i className="fa-regular fa-thumbs-up mx-[20px] text-[16px]"></i>
+              {/* <i class="fa-solid fa-thumbs-up"></i> */}
             </button>
           </div>
-          <div className="song-buttons-actions m-0 h-[20%] w-[100%] flex justify-center items-baseline">
+          <div className="song-buttons-actions m-0 h-[20%] w-[100%] flex justify-center items-center">
             <button>
-              <i className="fa-solid fa-circle  mx-[10px] text-[28px]"></i>
+              <span className="mx-[10px] text-[24px]">🔀</span>
             </button>
             <button>
-              <i className="fa-solid fa-circle mx-[10px] text-[28px]"></i>
+              <span className="mx-[10px] text-[24px]">⏪</span>
             </button>
             <button>
-              <i className="fa-solid fa-circle mx-[10px] text-[40px]"></i>
+              <span className="mx-[10px] text-[36px]">▶️</span>
             </button>
             <button>
-              <i className="fa-solid fa-circle mx-[10px] text-[28px]"></i>
+              <span className="mx-[10px] text-[24px]">⏩</span>
             </button>
             <button>
-              <i className="fa-solid fa-circle mx-[10px] text-[28px]"></i>
+              <span className=" mx-[10px] text-[24px]">🔁</span>
             </button>
           </div>
         </div>
