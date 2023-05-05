bc66f3dc1c32c59d026a402f0da98abd3271a7b7
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-04 19:34:04 -0600

    layout html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="background">
+      <div className="media-player">
+        <div className="song-cover-img">
+          <img src="" alt="song-cover" />
+        </div>
+        <div className="song-content">
+          <div className="song-copy">
+            <button>icon</button>
+            <h2>Song</h2>
+            <h3>artist</h3>
+          </div>
+          <div className="song-range">
+            <span>current time</span>
+            <input type="range"></input>
+            <span>total time</span>
+          </div>
+          <div className="song-buttons-actions">
+            <button>
+              <i>dislike</i>
+            </button>
+            <button>
+              <i>go back</i>
+            </button>
+            <button>
+              <i>resume</i>
+            </button>
+            button
+            <button>
+              <i>next song</i>
+            </button>
+            <button>
+              <i>like</i>
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
