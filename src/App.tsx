b1d2a2b8281c87cec12b166aa8c6f55e1175eb9a
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-05 00:10:38 -0600

    tailwind installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
@@ -10,31 +10,38 @@ function App() {
         </div>
         <div className="song-content">
           <div className="song-copy">
-            <button>icon</button>
+            <button>
+              <i className="fa-solid fa-circle"></i>
+            </button>
             <h2>Song</h2>
             <h3>artist</h3>
           </div>
           <div className="song-range">
-            <span>current time</span>
+            <button>
+              <i className="fa-solid fa-circle"></i>
+            </button>
+            <span>00:00</span>
             <input type="range"></input>
-            <span>total time</span>
+            <span>00:00</span>
+            <button>
+              <i className="fa-solid fa-circle"></i>
+            </button>
           </div>
           <div className="song-buttons-actions">
             <button>
-              <i>dislike</i>
+              <i className="fa-solid fa-circle"></i>
             </button>
             <button>
-              <i>go back</i>
+              <i className="fa-solid fa-circle"></i>
             </button>
             <button>
-              <i>resume</i>
-            </button>
-            button
-            <button>
-              <i>next song</i>
+              <i className="fa-solid fa-circle"></i>
             </button>
             <button>
-              <i>like</i>
+              <i className="fa-solid fa-circle"></i>
+            </button>
+            <button className="bg-blue-500 font-bold py-5 px-5 rounded-full">
+              <i className="fa-solid fa-circle"></i>
             </button>
           </div>
         </div>
