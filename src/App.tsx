2afd961227a6313f2763076e020274d87f716386
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-05 15:31:51 -0600

    adding tailwind styles for layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,45 @@ import { useState, useEffect } from "react";
 
 function App() {
   return (
-    <section className="background">
-      <div className="media-player">
-        <div className="song-cover-img">
+    <section className="background bg-orange-200 h-screen w-screen flex justify-center items-center relative m-0">
+      <div className="media-player h-[350px] w-[700px] bg-zinc-900 flex justify-evenly items-center relative  m-0 py-[20px] px-[0px] rounded-[20px] drop-shadow-[0px_0px_15px_rgba(0,0,0,.5)]">
+        <div className="song-cover-img w-[40%] h-[100%] object-cover bg-zinc-600 m-0 rounded-[20px]">
           <img src="" alt="song-cover" />
         </div>
-        <div className="song-content">
-          <div className="song-copy">
-            <button>
-              <i className="fa-solid fa-circle"></i>
+        <div className="song-content bg-zinc-600 w-[50%] h-[100%] m-0 flex justify-center items-center flex-col">
+          <div className="song-copy m-0  h-[60%] w-[100%]">
+            <button className="song-copy h-min w-min ml-auto flex">
+              <i className="fa-solid fa-circle my-[10px] mx-[10px] text-[30px]"></i>
             </button>
-            <h2>Song</h2>
-            <h3>artist</h3>
+            <h2 className="text-[30px] font-bold mt-[20px] px-[20px]">Song</h2>
+            <h3 className="text-[18px] italic px-[20px]">artist</h3>
           </div>
-          <div className="song-range">
-            <button>
-              <i className="fa-solid fa-circle"></i>
+          <div className="song-range m-0 h-[20%] w-[100%] flex justify-center items-center">
+            <button className="h-min w-min flex">
+              <i className="fa-solid fa-circle mx-[20px] text-[16px]"></i>
             </button>
-            <span>00:00</span>
+            <span className="mx-[10px] text-[12px]">00:00</span>
             <input type="range"></input>
-            <span>00:00</span>
+            <span className="mx-[10px] text-[12px]">00:00</span>
             <button>
-              <i className="fa-solid fa-circle"></i>
+              <i className="fa-solid fa-circle mx-[20px] text-[16px]"></i>
             </button>
           </div>
-          <div className="song-buttons-actions">
+          <div className="song-buttons-actions m-0 h-[20%] w-[100%] flex justify-center items-baseline">
             <button>
-              <i className="fa-solid fa-circle"></i>
+              <i className="fa-solid fa-circle  mx-[10px] text-[28px]"></i>
             </button>
             <button>
-              <i className="fa-solid fa-circle"></i>
+              <i className="fa-solid fa-circle mx-[10px] text-[28px]"></i>
             </button>
             <button>
-              <i className="fa-solid fa-circle"></i>
+              <i className="fa-solid fa-circle mx-[10px] text-[40px]"></i>
             </button>
             <button>
-              <i className="fa-solid fa-circle"></i>
+              <i className="fa-solid fa-circle mx-[10px] text-[28px]"></i>
             </button>
-            <button className="bg-blue-500 font-bold py-5 px-5 rounded-full">
-              <i className="fa-solid fa-circle"></i>
+            <button>
+              <i className="fa-solid fa-circle mx-[10px] text-[28px]"></i>
             </button>
           </div>
         </div>
