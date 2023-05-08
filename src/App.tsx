e09e14839bff8031a7d69aabde5c529ea04500fe
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-07 18:11:17 -0600

    hard coded data done, starting with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,65 @@
 import { useEffect, useState } from "react";
-// import "./App.css";
+import img1 from "./../ASSETS/cover-imgs/cover-img-song-1.jpg";
+import img2 from "./../ASSETS/cover-imgs/cover-img-song-2.jpg";
+import img3 from "./../ASSETS/cover-imgs/cover-img-song-3.jpg";
+import img4 from "./../ASSETS/cover-imgs/cover-img-song-4.jpg";
+import img5 from "./../ASSETS/cover-imgs/cover-img-song-5.jpg";
+import img6 from "./../ASSETS/cover-imgs/cover-img-song-6.jpg";
+import img7 from "./../ASSETS/cover-imgs/cover-img-song-7.jpg";
+import img8 from "./../ASSETS/cover-imgs/cover-img-song-8.jpg";
+import img9 from "./../ASSETS/cover-imgs/cover-img-song-9.jpg";
+import img10 from "./../ASSETS/cover-imgs/cover-img-song-10.jpg";
+import song1 from "./../ASSETS/songs/CustomMelody - Bad Guys.wav";
+import song2 from "./../ASSETS/songs/URL Melt - Unicorn Heads.mp3";
+import song3 from "./../ASSETS/songs/cyber by liquify Artlist.mp3";
+import song4 from "./../ASSETS/songs//cyber-runner by 2050 Artlist.mp3";
+import song5 from "./../ASSETS/songs/garage-sale by alex-zado Artlist.wav";
+import song6 from "./../ASSETS/songs/mon by brad-cane Artlist.mp3";
+import song7 from "./../ASSETS/songs/mythologica by ofrin Artlist.mp3";
+import song8 from "./../ASSETS/songs/rising-up by ofrin Artlist.mp3";
+import song9 from "./../ASSETS/songs/strength-men by lux-inspira Artlist.mp3";
+import song10 from "./../ASSETS/songs/zone by fvmeless Artlist.mp3";
 
+// prettier-ignore
 const data = [
   {
-    id: "1",
-    song: "idk",
-    artist: "idk",
-    src: "/",
-    genre: "idk",
+    id: "1", song: "Custom Melody", artist: "Bad Guys", src: song1,  cover: img7
+  },
+  {
+    id: "2", song: "URL Melt", artist: "Unicorn Heads", src: song2, cover: img3
+  },
+  {
+    id: "3", song: "Cyber", artist: "Liquify", src: song3, cover: img4
+  },
+  {
+    id: "4", song: "Cyber Runner", artist: "2050", src: song4, cover: img2
+  },
+  {
+    id: "5", song: "Garage Sale", artist: "Alex Zado", src: song5, cover: img10
+  },
+  {
+    id: "6", song: "Mon", artist: "Brad Cane", src:song6, cover: img6
+  },
+  {
+    id: "7", song: "Mythologica", artist: "Ofrin", src: song7, cover: img1  
+  },
+  {
+    id: "8", song: "Rising Up", artist: "Arlist", src: song8, cover: img9
+  },
+  {
+    id: "9", song: "Strenght Men", artist: "Lux Inspira", src: song9, cover: img8
+  },
+  {
+    id: "10", song: "Zone", artist: "Fvmeless", src: song10,  cover: img5
   },
 ];
-function App() {
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch("https://api.discogs.com/releases/249504");
-      const data = await response.json();
-      setData(data);
-    };
-    getData();
-  }, []);
+// prettier-ignore-end
 
+function App() {
   const [isSuffle, setSuffle] = useState(false);
   const [isPaused, setPaused] = useState(false);
   const [isRepeat, setRepeat] = useState("1");
-  const [isData, setData] = useState([{}]);
-
-  console.log(isData);
+  const [isSong, setSong] = useState({});
 
   const handleSuffle = () => {
     setSuffle(!isSuffle);
